b7fd7d11a8004c7a22a7a1de32862658b78d2354
Brian Devlin <brian.devlin@gmail.com>
2015-09-14 20:01:26 -0400

    recyclerviewadapter

---
+++ app/src/main/java/com/bdevlin/apps/pandt/RecyclerViewAdapter.java
@@ -0,0 +1,126 @@
+package com.bdevlin.apps.pandt;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+/**
+ * Created by bdevlin on 9/7/2015.
+ */
+
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ListItemViewHolder> {
+    private static final String TAG = RecyclerViewAdapter.class.getSimpleName();
+    private String[] mDataset;
+    /***** Creating OnItemClickListener *****/
+
+    // Define listener member variable
+    public  OnItemClickListener listener;
+    // Define the listener interface
+    public interface OnItemClickListener {
+        void onItemClick(View itemView, int position);
+    }
+    // Define the method that allows the parent activity or fragment to define the listener
+    public void setOnItemClickListener(OnItemClickListener listener) {
+        this.listener = listener;
+    }
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public static class ListItemViewHolder extends RecyclerView.ViewHolder
+            implements View.OnClickListener {
+        // each data item is just a string in this case
+        public TextView mTextView;
+        public TextView mLabel;
+        public ImageView mImage;
+        private Context context;
+        public IMyViewHolderClicks mListener;
+
+        public ListItemViewHolder(Context context, View itemLayoutView, IMyViewHolderClicks listener) {
+            super(itemLayoutView);
+            this.mListener = listener;
+            this.mTextView = (TextView) itemLayoutView.findViewById(R.id.textView);
+            this.mLabel = (TextView) itemLayoutView.findViewById(R.id.label);
+            this.mImage = (ImageView) itemLayoutView.findViewById(R.id.imageView2);
+            this.context = context;
+            mImage.setOnClickListener(this);
+            // Attach a click listener to the entire row view
+            itemLayoutView.setOnClickListener(this);
+
+        }
+
+        // Handles the row being  clicked
+        @Override
+        public void onClick(View v) {
+            int position = getLayoutPosition(); // gets item position
+            if (v instanceof ImageView){
+                mListener.onTomato((ImageView)v);
+            } else {
+                mListener.onPotato(v);
+            }
+            // Triggers click upwards to the adapter on click
+//            if (listener != null)
+//                listener.onItemClick(itemView, getLayoutPosition());
+            Toast.makeText(v.getContext(), "Id: " + getAdapterPosition(), Toast.LENGTH_LONG).show();
+        }
+
+        public static interface IMyViewHolderClicks {
+            public void onPotato(View caller);
+            public void onTomato(ImageView callerImage);
+        }
+    }
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    // in this case just a string[]
+    public RecyclerViewAdapter(String[] myDataset) {
+        mDataset = myDataset;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public ListItemViewHolder onCreateViewHolder(ViewGroup parent,
+                                                   int viewType) {
+        // create a new view
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.textview, parent, false);
+        // set the view's size, margins, paddings and layout parameters
+
+      // TextView vt =  (TextView)v.findViewById(R.id.textView);
+
+       // ListItemViewHolder vh = new ListItemViewHolder(parent.getContext(), v);
+        ListItemViewHolder vh = new ListItemViewHolder(parent.getContext(), v,
+                new RecyclerViewAdapter.ListItemViewHolder.IMyViewHolderClicks() {
+                    public void onPotato(View caller) { Log.d(TAG,"Poh-tah-tos"); };
+                    public void onTomato(ImageView callerImage) { Log.d(TAG,"To-m8-tohs"); }
+        });
+        return vh;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ListItemViewHolder holder, int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        holder.mTextView.setText(mDataset[position]);
+        holder.mLabel.setText(mDataset[position]);
+
+    }
+
+    @Override
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return mDataset.length;
+    }
+}
