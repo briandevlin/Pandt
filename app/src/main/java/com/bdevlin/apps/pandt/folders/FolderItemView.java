539d95d3bbee55d0fdeeda347718ec0ff6609488
briandevlin <brian.devlin@gmail.com>
2015-09-26 11:43:34 -0400

    folderitem

---
+++ app/src/main/java/com/bdevlin/apps/pandt/folders/FolderItemView.java
@@ -0,0 +1,25 @@
+package com.bdevlin.apps.pandt.folders;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+
+/**
+ * Created by bdevlin on 9/25/2015.
+ */
+public class FolderItemView  extends RelativeLayout {
+
+    public FolderItemView(Context context) {
+        super(context);
+    }
+
+
+    public FolderItemView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public FolderItemView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+
+    }
+}
